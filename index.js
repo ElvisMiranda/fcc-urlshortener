265264275bb0f250c415d84de71ab6a35aa4ff3a
Elvis Miranda <elvis.miranda@live.com>
2024-04-04 03:02:24 +0000

    Add solution to challenge

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const urlSchema = new mongoose.Schema({
+  original_url: String, 
+  short_url: String
+});
+
+const Url = mongoose.model('Url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -16,7 +31,56 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+  res.send({ greeting: 'hello API' });
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.post('/api/shorturl', async (req, res) => {
+  const url = req.body.url;
+
+  if (!url.startsWith('http')) {
+    res.send({ error: 'invalid url' });
+    return;
+  }
+
+  let docCount = await Url.countDocuments({});
+
+  await Url.findOne({ original_url: url })
+    .then(async (data) => {
+      if (data != null) {
+        res.send({ original_url: data.original_url, short_url: data.short_url });
+
+        return;
+      }
+
+      const newUrl = new Url({
+        original_url: url,
+        short_url: ++docCount
+      });
+
+      await newUrl.save()
+        .then((data) => { 
+          res.send({ original_url: data.original_url, short_url: data.short_url });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      
+    })
+    .catch((err) => { console.log(err); });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  Url.findOne({ short_url: req.params.short_url })
+    .then((url) => { 
+      if (url == null) {
+        res.status(404).json({ error: 'Not Found'});
+        return;
+      }
+      
+      res.redirect(url.original_url); 
+    })
+    .catch((err) => { console.log(err); })
 });
 
 app.listen(port, function() {
